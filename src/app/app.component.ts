f4def23cffee347a62bef13c979292d2aa24f0ad
markehost <mark.e.host@gmail.com>
2017-06-01 01:48:34 -0400

    basic UX, added functions to check for palindrome/highest palindrome value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
 import { Component } from '@angular/core';
+// import R from 'ramda';
+
+export class Palindrome {
+  min: string;
+  max: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  constructor() {
+
+    this.highest = this.highestAvailablePalindrome( this.palindrome )
+    
+  }
+
+  title = 'Highest Available Palindrome';
+
+  highest = null;
+
+  palindrome: Palindrome = {
+    min: '0',
+    max: '100',
+  };
+
+  isPalindrome( data: number ) {
+    // used solution from my practice repo
+    // https://github.com/markehost/javascript-exercises/tree/solutions/palindrome
+
+    let string = data.toString();
+
+    // REMOVE SPECIAL CHARACTERS - CONVERT TO LOWERCASE
+    let removeCharacter = string.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+
+    // REVERSE THE STRING SO WE CAN COMPARE VALUES
+    let reverseString = removeCharacter.split('').reverse().join('');
+
+    // CHECK IF STRINGS IS SAME FORWARDS AND BACKWARDS
+    return removeCharacter == reverseString;
+
+  };
+
+  highestAvailablePalindrome( range: { min: string, max: string } ) {
+    console.log(" highest available palindrome - range  ", range );
+
+    // this.isPalindrome( this.palindrome );
+    let min = parseInt( range.min, 10 );
+    let max = parseInt( range.max, 10 );
+    // for ( let i = range.max; i > range.min; i-- ) {
+    for ( let i = max; i > min; i-- ) {
+      console.log("what is i ---->", i);
+
+      let check = this.isPalindrome( i );
+      console.log("check ", check );
+
+      // if palindrome found - break out of loop
+      if ( check === true ) {
+        this.highest = i;
+        return i;
+        // break;
+      }
+    }
+  };
+
+  onChangeInput( event: any ) {
+    console.log(" on change input ", event.target.value );
+
+    this.highest = this.highestAvailablePalindrome( this.palindrome );
+  };
 }
