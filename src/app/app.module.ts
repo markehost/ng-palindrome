610e85c4328d297960c4591d432233fc9d70a315
markehost <mark.e.host@gmail.com>
2017-06-01 16:54:36 -0400

    pulled out palindrome, added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PalindromeComponent } from './palindrome.component';
+import { AboutComponent } from './about.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PalindromeComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/about',
+        pathMatch: 'full'
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'palindrome',
+        component: PalindromeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
